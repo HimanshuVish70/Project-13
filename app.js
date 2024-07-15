c3eb14c3ac601d34e83b6aafced6c6b2aed1fb25
Himanshu Kumar Vishwakarma <166942162+HimanshuVish70@users.noreply.github.com>
2024-07-15 14:06:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let body=document.querySelector("body");
+let pallete=document.querySelectorAll(".color_pallete");
+let h1=document.querySelector("h1");
+function changecolor(color){
+    body.style.backgroundColor=color;
+}
